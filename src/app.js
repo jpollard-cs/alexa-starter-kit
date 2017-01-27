afa0af0162f3d9c7c88d484af89bdaa74e18ae2f
jpollard-cs <jpollard@cspace.com>
2017-01-27 15:23:35 -0500

    simplified and added ability to use webpack tree shaking

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
-const launch = (app, options) => {
-    const { shouldEndSessionByDefault } = options;
-    const prompt = 'hello';
-    app.launch((req, res) =>  res.say(prompt).reprompt(prompt).shouldEndSession(shouldEndSessionByDefault))
+import Alexa from 'alexa-app';
+
+const options = {
+    shouldEndSessionByDefault: false
 };
 
-export default launch;
+const appName = 'alexa-starter-kit'; // TODO: change this to your app name
+const app = new Alexa.app(appName);
+
+const prompt = 'hello';
+app.launch((req, res) =>  res.say(prompt).reprompt(prompt).shouldEndSession(options.shouldEndSessionByDefault));
+
+// hack to support custom utterances in utterance expansion string
+// https://www.bignerdranch.com/blog/developing-alexa-skills-locally-with-nodejs-implementing-an-intent-with-alexa-app-and-alexa-app-server/
+const utterancesMethod = app.utterances;
+app.utterances = function() {
+    return utterancesMethod().replace(/\{\-\|/g, '{');
+};
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
+// IMPORTANT: this file must not use import statements (https://github.com/webpack/webpack/issues/4039)
+// as you can't mix import statements with module.exports when using webpack 2
+
 // Allow this module to be reloaded by hotswap when changed
 // useful if you're using alexa-app-server
 module.change_code = 1;
 
-import Alexa from 'alexa-app';
-import launch from './app';
-
-const options = {
-    shouldEndSessionByDefault: false
-};
-
-const appName = 'alexa-starter-kit'; // TODO: change this to your app name
-const app = new Alexa.app(appName);
-
-launch(app, options);
-
-// hack to support custom utterances in utterance expansion string
-// https://www.bignerdranch.com/blog/developing-alexa-skills-locally-with-nodejs-implementing-an-intent-with-alexa-app-and-alexa-app-server/
-const utterancesMethod = app.utterances;
-app.utterances = function() {
-    return utterancesMethod().replace(/\{\-\|/g, '{');
-};
+const app = require('./app').default;
 
 // this must use module.exports based on webpack config
-// and to be compattible with alexa-app-server
+// and to be compatible with alexa-app-server
 module.exports = app;
